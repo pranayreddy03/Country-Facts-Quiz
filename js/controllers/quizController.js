fcd7950d57ac6d2be40b639689d51e73ed520f81
pranay <reddy.pranay03@gmail.com>
2018-01-06 23:19:21 -0600

    Added change state to toggle between quiz and results page

--- js/controllers/quizController.js
+++ js/controllers/quizController.js
@@ -12,6 +12,8 @@
         quiz.quizMetrics = quizMetrics;
         quiz.dataService = dataService;
         quiz.activeQuestion = 0;
+        quiz.error = false;
+        quiz.finalize = false;
 
         var numOfQuestionsAnswered = 0;
 
@@ -26,6 +28,17 @@
                                         if(numOfQuestionsAnswered >= quizLength)
                                         {
                                             //finalize the quiz
+                                            for(var i=0; i< quiz.length;i++)
+                                            {
+                                                if(dataService.quizQuestions[i].selected === null)
+                                                {
+                                                    setActiveQuestion(i);
+                                                    return;
+                                                }
+                                            }
+                                            quiz.error = false;
+                                            quiz.finalize = true;
+                                            return;
                                         }
                                     }
                                     quiz.setActiveQuestion();
@@ -43,6 +56,11 @@
                 {
                     quiz.activeQuestion = quiz.activeQuestion < quizLength ? ++quiz.activeQuestion : 0;
 
+                    if(quiz.activeQuestion === 0)
+                    {
+                        quiz.error = true;
+                    }
+
                     if(dataService.quizQuestions[quiz.activeQuestion].selected === null)
                     {
                         breakOut = true;
@@ -59,6 +77,16 @@
         quiz.selectAnswer = function(index)
         {
             dataService.quizQuestions[quiz.activeQuestion].selected = index;
+        };
+
+        quiz.finalizeAnswers = function()
+        {
+            quiz.finalize = false;
+            numOfQuestionsAnswered = 0;
+            quiz.activeQuestion = 0;
+            quizMetrics.markQuiz();
+            quizMetrics.changeState("quiz", false);
+            quizMetrics.changeState("results", true);
         }
     }
 
