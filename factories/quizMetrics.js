8b540b0e1d2b16250d87b0fb33f9ce008634e94a
pranay <reddy.pranay03@gmail.com>
2018-01-06 23:15:19 -0600

    Added function to check correct answers

--- factories/quizMetrics.js
+++ factories/quizMetrics.js
@@ -5,16 +5,51 @@
     angular.module('countryFacts')
            .factory('quizMetrics',quizMetrics);
 
-    function quizMetrics()
+    quizMetrics.$inject = ['dataService'];
+    function quizMetrics(dataService)
     {
-        var factory = {
+        var quizMetricsfactory = {
                         quizActive : false,
-                        changeState : function changeState(state)
+                        resultsActive : false,
+                        changeState : function changeState(metric, state)
                                        {
-                                                factory.quizActive = state;
-                                       }
+                                           if(metric === "quiz")
+                                           {
+                                               quizMetricsfactory.quizActive = state;
+                                           }
+                                           else if(metric === "results")
+                                           {
+                                               quizMetricsfactory.resultsActive = state;
+                                           }
+                                           else
+                                           {
+                                               return false;
+                                           }
+                                       },
+                        correctAnswers : [],
+                        markQuiz: markQuiz,
+                        numOfCorrect : 0
                       };
-        return factory;
+        return quizMetricsfactory;
+
     }
+
+    function markQuiz()
+    {
+        quizMetricsfactory.correctAnswers = dataService.correctAnswers;
+        for(var i=0; i< dataService.quizQuestions.length;i++)
+        {
+            if(dataService.quizQuestions[i].selected === dataService.correctAnswers[i])
+            {
+                dataService.quizQuestions[i].correct = true;
+                quizMetricsfactory.numOfCorrect++;
+            }
+            else
+            {
+                dataService.quizQuestions[i].correct = true;
+            }
+        }
+    }
+
 
 })();
