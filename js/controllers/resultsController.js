930f83cc700c0d6ca857a737ffcc362b65a53ea7
pranay <reddy.pranay03@gmail.com>
2018-01-07 15:23:42 -0600

    Added Functionalities to reset the App, calculate the percentage

--- js/controllers/resultsController.js
+++ js/controllers/resultsController.js
@@ -9,5 +9,41 @@
     {
         var result = this;
         result.quizMetrics = quizMetrics;
+        result.dataService = dataService;
+        result.activeQuestion = 0;
+        result.getAnswerClass = function(index)
+        {
+                 if(index === quizMetrics.correctAnswers[result.activeQuestion])
+                 {
+                     return "bg-success";
+                 }
+                 else if(index === dataService.quizQuestions[result.activeQuestion].selected)
+                 {
+                     return "bg-danger";
+                 }
+        };
+
+        result.setActiveResultQuestion = function(index)
+        {
+            result.activeQuestion = index;
+        };
+
+        result.calculatePercentage = function()
+        {
+            return quizMetrics.numOfCorrect / dataService.quizQuestions.length * 100;
+        };
+
+        result.reset = function()
+        {
+            quizMetrics.changeState('results', false);
+            quizMetrics.numOfCorrect = 0;
+
+            for(var i=0; i< dataService.quizQuestions.length;i++)
+            {
+                var data = dataService.quizQuestions[i];
+                data.selected = null;
+                data.correct = null;
+            }
+        }
     }
 })();
