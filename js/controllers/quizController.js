d24442e967fe6de233c9e35ed4918b8817069fca
pranay <reddy.pranay03@gmail.com>
2018-01-06 18:42:54 -0600

    Added functionality for continue button to move onto next question

--- js/controllers/quizController.js
+++ js/controllers/quizController.js
@@ -5,11 +5,48 @@
     angular.module('countryFacts')
            .controller('quizController',quizController);
 
-    quizController.$inject = ['quizMetrics'];
-    function quizController(quizMetrics)
+    quizController.$inject = ['quizMetrics', 'dataService'];
+    function quizController(quizMetrics, dataService)
     {
         var quiz = this;
         quiz.quizMetrics = quizMetrics;
+        quiz.dataService = dataService;
+        quiz.activeQuestion = 0;
+
+        var numOfQuestionsAnswered = 0;
+
+        quiz.questionAnswered = function()
+                                {
+                                    console.log('one');
+                                    var quizLength = dataService.quizQuestions.length;
+                                    if(dataService.quizQuestions[quiz.activeQuestion].selected !== null)
+                                    {
+                                        numOfQuestionsAnswered++;
+
+                                        if(numOfQuestionsAnswered >= quizLength)
+                                        {
+                                            //finalize the quiz
+                                        }
+                                    }
+                                    quiz.setActiveQuestion();
+                                };
+
+        quiz.setActiveQuestion = function()
+        {
+            console.log('two');
+            var breakOut = false;
+            var quizLength = dataService.quizQuestions.length - 1;
+
+            while(!breakOut)
+            {
+                quiz.activeQuestion = quiz.activeQuestion < quizLength ? ++quiz.activeQuestion : 0;
+
+                if(dataService.quizQuestions[quiz.activeQuestion].selected === null)
+                {
+                    breakOut = true;
+                }
+            }
+        }
     }
 
 })();
